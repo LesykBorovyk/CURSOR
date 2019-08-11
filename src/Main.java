5a8ab18fa80d08351160d6bae08f7585f001e4a6
Lesyk <Ckeell5g>
2019-08-11 20:09:13 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         su_15.engine_on();
         su_15.fly_up();
         su_15.attack(Destroyer.AttackType.GUIDED);
+        su_15.attack(Destroyer.AttackType.GUIDED);
+        su_15.attack(Destroyer.AttackType.GUIDED);
         su_15.attack(Destroyer.AttackType.NOT_GUIDED);
         su_15.landing();
         su_15.engine_off();
