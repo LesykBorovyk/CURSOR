94d11bf30862db62f31107404cef9f0f5902fcab
Lesyk <Ckeell5g>
2019-08-14 03:05:31 +0300

    added menu for selecting task; added method 'onSortIntergersSelected'; took away from 'onArithmeticMeanSelected' method operators that fill collection and that print collection and made separate methods with same functionality; added to method 'onSumPositiveNumbers' condition that reads string if it is;

--- CURSOR2/src/HW2.java
+++ CURSOR2/src/HW2.java
@@ -1,16 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class HW2 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-//        onArithmeticMeanSelected(scan);
-//        onSumPositiveNumbers(scan);
+        startConversation(scan);
 
+    }
 
+    private static void startConversation(Scanner scan) {
+        String hub;
+        System.out.println("Hello! Choose one of methods:");
+        System.out.println("1.onSortIntegersSelected");
+        System.out.println("2.onArithmeticMeanSelected");
+        System.out.println("3.onSumPositiveNumbers");
+        do {
+            switch (hub = scan.nextLine()) {
+                case "1":
+                    onSortIntegersSelected(scan);
+                    break;
+                case "2":
+                    onArithmeticMeanSelected(scan);
+                    break;
+                case "3":
+                    onSumPositiveNumbers(scan);
+                    break;
+                case "exit":
+                    System.out.println("Goodbye");
+                    break;
+                default:
+                    System.out.println("wrong number");
+                    System.out.println("Please enter number from list:");
+                    break;
+            }
+        } while (!hub.equalsIgnoreCase("exit"));
     }
 
     private static int calculate(int[] arr) {
@@ -20,7 +44,7 @@ public class HW2 {
                 sum += anArr;
             }
         }
-        System.out.println("sum of positive numbers:");
+        System.out.print("sum of positive numbers: ");
         return sum;
     }
 
@@ -32,9 +56,53 @@ public class HW2 {
         return sum / arrList.size();
     }
 
+    private static void onSortIntegersSelected(Scanner scanner) {
+        TreeSet<Integer> set = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                boolean isFirstNumberEven = o1 % 2 == 0;
+                boolean isSecondNumberEven = o2 % 2 == 0;
+                if (isFirstNumberEven && !isSecondNumberEven) {
+                    return -1;
+                } else if (!isFirstNumberEven && isSecondNumberEven) {
+                    return 1;
+                } else if (isFirstNumberEven && isSecondNumberEven) {
+                    return o1 - o2;
+                } else {
+                    return o1 - o2;
+                }
+            }
+        });
+        fillCollection(set, scanner);
+        printCollection(set);
+    }
+
     private static void onArithmeticMeanSelected(Scanner scan) {
         ArrayList<Integer> arrlist = new ArrayList<>();
 
+        fillCollection(arrlist, scan);
+        printCollection(arrlist);
+        System.out.println("arithmetic mean of your array: " + mid(arrlist));
+    }
+
+    private static void printCollection(Collection<Integer> arrlist) {
+        if (arrlist.isEmpty()) {
+            System.out.print("Collection is empty");
+            return;
+        }
+        System.out.print("your array: ");
+        Iterator<Integer> iterator = arrlist.iterator();
+        do {
+            System.out.print(iterator.next());
+            if (iterator.hasNext()) {
+                System.out.print(", ");
+            } else {
+                System.out.println();
+            }
+        } while (iterator.hasNext());
+    }
+
+    private static void fillCollection(Collection<Integer> arrlist, Scanner scan) {
         System.out.println("Enter integers and then enter 'done':");
         do {
             if (scan.hasNextInt()) {
@@ -43,28 +111,20 @@ public class HW2 {
         } while (!scan.nextLine().equalsIgnoreCase("done"));
 
         System.out.println();
-
-        System.out.print("your array: ");
-        for (int i = 0; i < arrlist.size(); i++) {
-            System.out.print(arrlist.get(i));
-            if (i != arrlist.size() - 1) {
-                System.out.print(", ");
-            } else {
-                System.out.println();
-            }
-        }
-        System.out.println("arithmetic mean of your array: " + mid(arrlist));
     }
 
-    private static void onSumPositiveNumbers(Scanner scan){
-        System.out.println("Enter array capacity");
+    private static void onSumPositiveNumbers(Scanner scan) {
+        System.out.print("Enter array capacity = ");
         int[] arr = new int[scan.nextInt()];
 
         for (int j = 0; j < arr.length; j++) {
-            System.out.println("array [" + j + "] =");
+            System.out.print("array [" + j + "] = ");
             arr[j] = scan.nextInt();
         }
 
-        System.out.println(calculate(arr));
+        System.out.print(calculate(arr));
+        if (scan.hasNextLine()) {
+            scan.nextLine();
+        }
     }
 }
